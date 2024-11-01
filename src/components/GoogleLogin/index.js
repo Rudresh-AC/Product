6f316ce426a2dcea44d2c5edc4455360359c880a
Rudresh A C <rudreshac@Rudreshs-MacBook-Air.local>
2024-11-01 15:16:39 +0530

    added the gogle signin

--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import {styles} from './styles';
+import {
+  GoogleSignin,
+  statusCodes,
+  isErrorWithCode,
+} from '@react-native-google-signin/google-signin';
 
 const GoogleLogin = () => {
+  const handleGoogleLogin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
   return (
-    <TouchableOpacity activeOpacity={0.6} style={styles.container}>
+    <TouchableOpacity
+      activeOpacity={0.6}
+      style={styles.container}
+      onPress={handleGoogleLogin}>
       <Image style={styles.image} source={require('../../assets/google.png')} />
     </TouchableOpacity>
   );
